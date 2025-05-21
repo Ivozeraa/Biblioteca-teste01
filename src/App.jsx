ed4a3ac0f97d0657db7b3f3ff295a7d64d6b18ce
Ivozeraa <joseivo2008@gmail.com>
2025-05-21 09:24:27 -0300

    Text - Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,10 @@
-import { useState } from 'react';
-import Livro from './classes/Livro';
-import { Aluno, Professor } from './classes/Aluno';
-import Biblioteca from './classes/Biblioteca';
-import FormLivro from "./components/FormLivroTEMP";
-import FormUsuario from './components/FormUsuario';
-import ListaLivros from './components/ListaLivros';
-import ListaUsuarios from './components/ListaUsuarios';
-import Emprestimo from './components/Emprestimo';
+import { Main } from './pages/Main';
 
-const biblioteca = new Biblioteca();
 
 export default function App() {
-  const [livros, setLivros] = useState([]);
-  const [usuarios, setUsuarios] = useState([]);
-
-  function addLivro({ titulo, autor, isbn }) {
-    const novoLivro = new Livro(titulo, autor, isbn);
-    biblioteca.cadastrarLivro(novoLivro);
-    setLivros([...biblioteca.livros]);
-  }
-
-  function addUsuario({ nome, id, tipo }) {
-    const novoUsuario = tipo === 'Aluno' ? new Aluno(nome, id) : new Professor(nome, id);
-    biblioteca.cadastrarUsuario(novoUsuario);
-    setUsuarios([...biblioteca.usuarios]);
-  }
-
-  function handleEmprestimo(usuarioId, isbn) {
-    try {
-      biblioteca.realizarEmprestimo(usuarioId, isbn);
-      setLivros([...biblioteca.livros]);
-      setUsuarios([...biblioteca.usuarios]);
-      alert('Empréstimo realizado com sucesso');
-    } catch (e) {
-      alert(e.message);
-    }
-  }
-
   return (
     <div style={{ padding: 20 }}>
-      <h1>Biblioteca Digital React</h1>
-      <FormLivro onAddLivro={addLivro} />
-      <FormUsuario onAddUsuario={addUsuario} />
-      <Emprestimo usuarios={usuarios} livros={livros} onEmprestar={handleEmprestimo} />
-      <ListaLivros livros={livros} />
-      <ListaUsuarios usuarios={usuarios} />
+      <Main />
     </div>
   );
 }
--- src/components/FormLivro.jsx
+++ src/components/FormLivro.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Styles from '../styles/formlivro.module.css'
 
 export default function FormLivro({ onAddLivro }) {
   const [titulo, setTitulo] = useState('');
@@ -14,7 +15,7 @@ export default function FormLivro({ onAddLivro }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <h3>Cadastrar Livro</h3>
+      <h3 class={Styles.title}>Cadastrar Livro</h3>
       <input placeholder="Título" value={titulo} onChange={e => setTitulo(e.target.value)} />
       <input placeholder="Autor" value={autor} onChange={e => setAutor(e.target.value)} />
       <input placeholder="ISBN" value={isbn} onChange={e => setIsbn(e.target.value)} />
