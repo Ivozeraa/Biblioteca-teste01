0e8d095ac36216a9fde424c476067254ee09c3a3
Erik3331 <erikramon3331@gmail.com>
2025-05-31 12:17:22 -0300

    nd

--- src/components/Carrossel.jsx
+++ src/components/Carrossel.jsx
@@ -2,7 +2,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-import img1 from '../assets/carrossel/img1.jfif';
+import img1 from '../assets/carrossel/img1.jpg';
 
 import S from './styles/Carrossel.module.css';
 
