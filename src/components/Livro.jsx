635f61cab7a654f08191c9f5e00fc78829e861ff
Erik3331 <erikramon3331@gmail.com>
2025-05-31 12:15:17 -0300

    att carrossel att componente livro

--- src/components/Livro.jsx
+++ src/components/Livro.jsx
@@ -1,9 +1,11 @@
 import S from './styles/Livro.module.css'
 
-export const Livro = () => {
+export const Livro = ({titulo, editora, isbn} = props) => {
   return (
     <div className={S.livro}>
-      
+      <h2>{titulo}</h2>
+      <p>{editora}</p>
+      isbn
     </div>
   )
 }
